0db07c1e8b5310ad80849768df6340fa0f44ac0b
Guy MANDINA NZEZA <guyghost@gmail.com>
2023-01-18 23:51:16 +0100

    chore: format code

--- src/lib/components/molecules/Player/Player.stories.svelte
+++ src/lib/components/molecules/Player/Player.stories.svelte
@@ -15,6 +15,7 @@
 		url: 'https://www.youtube.com/embed/G-ECTntZYtk',
 		title: 'Olivia Mabounga',
 		subtitle: 'Episode 1',
-        overview: "Olivia Mabounga, française d'origine Algérienne et Congolaise, a écrit et mis en scène Tchoko, un spectacle qui parle du phénomène de l'éclaircissement de la peau chez les femmes noires interprété par Benedicte Mbemba au Théâtre du Lavoir Moderne Parisien sur fond de texte rap, danse chant et pop culture."
+		overview:
+			"Olivia Mabounga, française d'origine Algérienne et Congolaise, a écrit et mis en scène Tchoko, un spectacle qui parle du phénomène de l'éclaircissement de la peau chez les femmes noires interprété par Benedicte Mbemba au Théâtre du Lavoir Moderne Parisien sur fond de texte rap, danse chant et pop culture."
 	}}
 />
