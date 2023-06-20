8cc7c2a1df68edf649fab0863af4a770bf4b43f8
Guy MANDINA NZEZA <guyghost@gmail.com>
2023-06-20 16:53:41 +0200

    chore: :wrench: update conf files

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,6 @@ const config = {
 	}),
 	kit: {
 		adapter: adapter()
-	},
-	package: {
-		emitTypes: true,
-		exports: (filepath) => !/^_|\/_|\.d\.ts$|\.stories\.svelte$/.test(filepath),
-		files: () => true
 	}
 };
 
