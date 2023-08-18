59ec34d71859b118fe069598bdf7d59a4f42ce22
Guy MANDINA NZEZA <guyghost@gmail.com>
2023-08-19 00:05:23 +0200

    feat: add pathline component

---
+++ src/lib/components/molecules/PathLine/PathLine.stories.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import PathLine from './PathLine.svelte';
+</script>
+
+<Meta
+	title="Molecules/PathLine"
+	component={PathLine}
+	argTypes={{
+		order: { control: 'text' },
+		title: { control: 'text' },
+		color: { control: 'text' }
+	}}
+/>
+
+<Template let:args>
+	<PathLine {...args} />
+</Template>
+
+<Story
+	name="Green PathLine"
+	args={{
+        title: 'Simple',
+		order: '1',
+		color: '#22c55e'
+	}}
+/>
+
+<Story
+	name="Yellow PathLine"
+	args={{
+        title: 'Transparente',
+        order: '2',
+        color: '#eab308'
+    }}
+/>
+
+<Story
+	name="Blue PathLine"
+	args={{
+        title: 'Révolutionnaire',
+        order: '3',
+        color: '#3b82f6'
+    }}
+/>
---
+++ src/lib/components/molecules/PathLine/index.ts
@@ -0,0 +1,1 @@
+export { default as PathLine } from './PathLine.svelte';
