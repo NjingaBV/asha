b6a2e09e5dd78181447e7737cfa210ffd55b3153
Guy MANDINA NZEZA <guyghost@gmail.com>
2023-08-22 12:18:14 +0200

    Feat/clean unused libraries (#85)
    deps: remove unused libraries

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,11 @@
 const path = require('path');
 module.exports = {
-	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-svelte-csf',
-		{
-			name: '@storybook/addon-postcss',
-			options: {
-				postcssLoaderOptions: {
-					implementation: require('postcss')
-				}
-			}
-		},
-		'@storybook/addon-mdx-gfm'
+		'@storybook/addon-styling'
 	],
 	framework: {
 		name: '@storybook/sveltekit',
